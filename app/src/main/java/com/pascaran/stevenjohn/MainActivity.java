445d2d67c3a0855d83feabffbc3afeaf567d9d75
unknown <PASCARAN_STEVEN_115@IICSGENERAL.COM>
2018-09-25 14:29:55 +0800

    Initial commit

---
+++ app/src/main/java/com/pascaran/stevenjohn/MainActivity.java
@@ -0,0 +1,62 @@
+package com.pascaran.stevenjohn;
+
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("4ITG","onCreate has executed...");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("start","onStart has executed...");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("resume","onResume has executed...");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("pause","onPause has executed...");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("stop","onStop has executed...");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("restart","onRestart has executed...");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("destroy","onDestroy has executed...");
+    }
+
+    public void showMessage(View v){
+        Toast.makeText(this,"4ITG is the best...",Toast.LENGTH_LONG).show();
+    }
+
+    public void showMessageSnack(View v){
+        Snackbar.make(v,"Prelim is fast approaching...",Snackbar.LENGTH_LONG).show();
+    }
+}
